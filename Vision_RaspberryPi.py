7d0b02f545e0be2a51e407019d25245af52a7cbd
Rishivarshil <69435368+Rishivarshil@users.noreply.github.com>
2021-01-29 16:40:01 -0500

    Add files via upload

--- Vision_RaspberryPi.py
+++ Vision_RaspberryPi.py
@@ -1,129 +1,64 @@
-from collections import deque
-from imutils.video import VideoStream
+
 import numpy as np
-import argparse
-import cv2
-import imutils
+import serial
+from serial import Serial
 import time
+import imutils
+#import sys
+import cv2
+# import os
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-    help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=32,
-    help="max buffer size")
-args = vars(ap.parse_args())
-
+#Setup Communication path for arduino (put the port name)
+arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600)
+time.sleep(2)
+print("Connected to Arduino...")
 
 yellowLower = (20, 100, 100)
 yellowUpper = (30, 255, 255)
+# cv2_base_dir = os.path.dirname(os.path.abspath(cv2.__file__))
+# haar_model = os.path.join(cv2_base_dir, 'data/haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-pts = deque(maxlen=args["buffer"])
-counter = 0
-(dX, dY) = (0, 0)
-direction = ""
+cap = cv2.VideoCapture(0)
+print("Getting camera image...")
+while 1:
 
-
-if not args.get("video", False):
-    vs = VideoStream(src=0).start()
-
-else:
-    vs = cv2.VideoCapture(args["video"])
-
-time.sleep(2.0)
-
-
-while True:
-
-    frame = vs.read()
-
-    frame = frame[1] if args.get("video", False) else frame
-
-
-    if frame is None:
-        break
-
-
-    frame = imutils.resize(frame, width=600)
-   # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-
-    mask = cv2.inRange(frame, yellowLower, yellowUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-
+    ret, img = cap.read()
+    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+  
+    gray  = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+    
 
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
         cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     center = None
-
     if len(cnts) > 0:
         
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
         if radius > 10:
-            
-            cv2.circle(frame, (int(x), int(y)), int(radius),
+
+            cv2.circle(img, (int(x), int(y)), int(radius),
                 (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-            pts.appendleft(center)
+            cv2.circle(img, center, 5, (0, 0, 255), -1)
+  
+ #sending data to arduino
+        print("Center of Rectangle is :", center)
+        data = "X{0:d}Y{1:d}Z".format(int(x), int(y))
+        print ("output = '" +data+ "'")
+        arduino.write(data.encode())
 
-    for i in np.arange(1, len(pts)):
-            
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        
-        if counter >= 10 and i == 10 and pts[i-10] is not None:
+    cv2.imshow('img',img)
+    cv2.imshow('idk',mask)
     
-            dX = pts[i-10][0] - pts[i][0]
-            dY = pts[i-10][1] - pts[i][1]
-            (dirX, dirY) = (0,0)
 
 
-            if np.abs(dX) > 10:
-                dirX = dX
-                #if dX is positive, the robot needs to move to the right
-                #if dX is negative, the robot needs to move to the left
-        
-            if np.abs(dY) > 10:
-                dirY = dY
-                #if dY is positive, the robot needs to move down
-                #if dY is negative, the robot needs to move up 
-            if dirX != "" and dirY != "":
-                direction = "{}-{}".format(dirY, dirX)
-            else:
-                direction = dirX if dirX != "" else dirY
-        
-        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-    
-    cv2.putText(frame, direction, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-        0.65, (0, 0, 255), 3)
-    cv2.putText(frame, "dx: {}, dy: {}".format(dX, dY),
-        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-        0.35, (0, 0, 255), 1)
-
-
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    counter += 1
-
-
-    if key == ord("q"):
-        break
-
-
-if not args.get("video", False):
-    vs.stop()
-
-else:
-    vs.release()
-
-
-cv2.destroyAllWindows()
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+       break
